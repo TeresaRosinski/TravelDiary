9b9ba69d35dae37f281ee63cc8f404b383ecf33e
Teresa Rosinski <TeresaRosinski@users.noreply.github.com>
2020-07-22 19:41:44 -0500

    add all initial files

---
+++ TravelDiary/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+//This is a copied template from epicodus
+using Microsoft.AspNetCore.Mvc;
+using TravelDiary.Models;
+
+namespace ToDoList.Controllers
+{
+  public class HomeController : Controller
+  {
+
+
+  }
+}
